e412d84f78615cc11414fa35d8c4ad7874738a0d
inb <inorton@gmail.com>
2024-12-15 10:54:54 +0000

    engineering mode WIP

--- content/scripts/library_custom_1.lua
+++ content/scripts/library_custom_1.lua
@@ -7,16 +7,38 @@
 -- added by the alternate loadout, you wont be permitted to re-add it.
 
 function custom_dynamic_vehicle_loadout_rows(vehicle, dynamic)
+	local opt, fitted = rev_get_custom_upgrade_option(vehicle)
+	local rows = {}
+	if opt and fitted then
+		for i, r in pairs(opt.rows) do
+			if r ~= nil then
+				rows[i] = r
+			end
+		end
+		return rows
+	end
+
 	return dynamic
 end
 
 function custom_dynamic_vehicle_loadout_options(vehicle, dynamic, attachment_index)
+	local replaced_opts = nil
+	local opt, fitted = rev_get_custom_upgrade_option(vehicle)
+	if opt and fitted then
+		replaced_opts = opt.options[attachment_index]
+	end
+
+	if replaced_opts ~= nil then
+		dynamic = replaced_opts
+	end
+
 	return dynamic
 end
 
 
 function rev_engineering_can_upgrade(vehicle)
-	return rev_get_custom_upgrade_option(vehicle) ~= nil
+	local opt, fitted = rev_get_custom_upgrade_option(vehicle)
+	return opt and not fitted
 end
 
 g_prompt_upgrade_vehicle = nil
@@ -26,11 +48,22 @@ function rev_get_custom_upgrade_option(vehicle)
 		local def = vehicle:get_definition_index()
 		for _, value in pairs(g_revolution_crafting_items) do
 			if value.chassis == def then
-				return value
+				-- if a special option isnt fitted
+				local fitted = false
+				for anum, adef in pairs(value.attachments) do
+					local a = vehicle:get_attachment(anum)
+					if a and a:get() then
+						local a_fitted = a:get_definition_index()
+						if a_fitted == adef then
+							fitted = true
+						end
+					end
+				end
+				return value, fitted
 			end
 		end
 	end
-	return nil
+	return nil, nil
 end
 
 
@@ -45,7 +78,7 @@ end
 function custom_vehicle_loadout_update(screen_w, screen_h, ticks)
 	if g_prompt_upgrade_vehicle then
 		local vehicle = update_get_map_vehicle_by_id(g_prompt_upgrade_vehicle)
-		local upgrade_option = rev_get_custom_upgrade_option(vehicle)
+		local upgrade_option, fitted = rev_get_custom_upgrade_option(vehicle)
 
 		if upgrade_option ~= nil then
 			local carrier = get_managed_vehicle()
@@ -55,7 +88,7 @@ function custom_vehicle_loadout_update(screen_w, screen_h, ticks)
 			local window = ui:begin_window("Engineering", 0, 0, screen_w, screen_h, nil, true, 1)
 			-- title
 			update_ui_rectangle(0, 0, screen_w, 14, color_white)
-			window.cy = 3 + update_ui_text(0, 4, "UPGRADE UNIT", screen_w, 1, color_black, 0)
+			window.cy = 3 + update_ui_text(0, 4, "UPGRADE UNIT?", screen_w, 1, color_black, 0)
 			local v_def = vehicle:get_definition_index()
 
 			-- body
@@ -65,11 +98,18 @@ function custom_vehicle_loadout_update(screen_w, screen_h, ticks)
 			ui:text_basic("> " .. upgrade_option.name)
 			ui:text_basic(upgrade_option.details)
 			ui:text_basic("COST:")
+			local has_reqs = true
 			for inv_item, inv_count in pairs(upgrade_option.cost) do
-				ui:text_basic(string.format("%d x %s", inv_count, g_item_data[inv_item].name))
+				local has_count = carrier:get_inventory_count_by_item_index(inv_item)
+				local col = color_grey_dark
+				if has_count < inv_count then
+					col = color_status_dark_red
+					has_reqs = false
+				end
+				ui:text_basic(string.format("%dx %s", inv_count, g_item_data[inv_item].name), col)
 			end
 
-			if ui:button("upgrade", true, 1) then
+			if ui:button("APPLY", has_reqs, 1) then
 				g_prompt_upgrade_vehicle = nil
 				if update_get_is_focus_local() then
 					-- do the upgrade
@@ -91,17 +131,14 @@ function custom_vehicle_loadout_update(screen_w, screen_h, ticks)
 end
 
 function custom_ui_vehicle_loadout_chassis(ui, vehicle)
-	if rev_engineering_can_upgrade(vehicle) then
-		if ui.prompt_upgrade then
-			ui:text("hello world foo bar")
-		else
-			rev_custom_button(ui, "UPGRADE",
-					4, 72, 47, 18, true, function()
-						if vehicle and vehicle:get() then
-							g_prompt_upgrade_vehicle = vehicle:get_id()
-						end
-					end)
-		end
+	local opt, fitted = rev_get_custom_upgrade_option(vehicle)
+	if opt ~= nil then
+		rev_custom_button(ui, "REFIT",
+				4, 72, 47, 18, not fitted, function()
+					if vehicle and vehicle:get() then
+						g_prompt_upgrade_vehicle = vehicle:get_id()
+					end
+				end)
 	else
 		g_prompt_upgrade_vehicle = nil
 	end
@@ -115,11 +152,13 @@ local g__crafting_reduced_wing = {
 	e_game_object_type.attachment_hardpoint_missile_ir,
 	e_game_object_type.attachment_hardpoint_missile_laser,
 	e_game_object_type.attachment_hardpoint_missile_aa,
-	e_game_object_type.attachment_hardpoint_missile_tv,
-	e_game_object_type.attachment_hardpoint_torpedo,
 	e_game_object_type.attachment_hardpoint_torpedo_noisemaker,
-	e_game_object_type.attachment_hardpoint_torpedo_decoy,
 	e_game_object_type.fuel_tank_aircraft,
+}
+
+local g__crafting_aa_reduced_wing = {
+	e_game_object_type.attachment_hardpoint_missile_aa,
+	e_game_object_type.attachment_hardpoint_missile_tv,
 }
 
 local g__crafting_aa_extra_wing = {
@@ -127,8 +166,9 @@ local g__crafting_aa_extra_wing = {
 }
 
 local g__crafting_flare_wing = {
-	e_game_object_type.fuel_tank_aircraft,
-	e_game_object_type.flare_launcher,
+	e_game_object_type.attachment_fuel_tank_plane,
+	e_game_object_type.attachment_flare_launcher,
+	e_game_object_type.attachment_smoke_launcher_explosive,
 }
 
 g_revolution_crafting_items = {
@@ -143,46 +183,85 @@ g_revolution_crafting_items = {
     --  - 1x razorbill
     --  - 1x flare launcher
     {
-		name="Petrel S1",
-		details="Anti-ship & AEW",
+		name="Specops Petrel",
+		details="Flare & Virus",
         chassis=e_game_object_type.chassis_air_rotor_heavy,
         attachments={
-            [1] = e_game_object_type.attachment_radar_golfball,
+            [4] = e_game_object_type.attachment_turret_robot_dog_capsule,
 		},
 		options={
-			[1] = {},
-			[2] = g__crafting_reduced_wing,
-			[3] = g__crafting_reduced_wing,
-			[4] = g__crafting_reduced_wing,
-			[5] = g__crafting_flare_wing,
+			[1] = {
+				e_game_object_type.attachment_camera_plane
+			},
+			[4] = {e_game_object_type.attachment_turret_robot_dog_capsule},
+			[2] = {
+				e_game_object_type.attachment_turret_plane_chaingun,
+				e_game_object_type.attachment_fuel_tank_plane,
+			},
+			[5] = {
+				e_game_object_type.attachment_flare_launcher,
+				e_game_object_type.attachment_smoke_launcher_explosive,
+			},
+			[3] = {
+				e_game_object_type.attachment_turret_plane_chaingun,
+				e_game_object_type.attachment_fuel_tank_plane,
+			},
 		},
-		rows={},
+		rows={
+			{
+                { i=1, x=0, y=-22 }
+            },
+            {
+                { i=2, x=-20, y=0 },
+                { i=4, x=-10, y=0 },
+                { i=5, x=10, y=0 },
+                { i=3, x=20, y=0 }
+            }
+		},
         cost={
-            [e_inventory_item.vehicle_rotor_light] = 1,
-            [e_inventory_item.attachment_flare_launcher] = 1,
+            [e_inventory_item.fuel_barrel] = 1,
+            [e_inventory_item.virus_module] = 1,
         }
     },
 	{
 		name="Manta F2",
-		details="Air combat",
+		details="Extra missiles",
 		chassis=e_game_object_type.chassis_air_wing_heavy,
 		attachments={
 			[10] = e_game_object_type.attachment_camera_observation,
 		},
 		options={
+			[4] = g__crafting_aa_reduced_wing,
+			[5] = g__crafting_aa_reduced_wing,
 			[8] = g__crafting_aa_extra_wing,
 			[9] = g__crafting_aa_extra_wing,
+			[10] = {
+				e_game_object_type.attachment_camera_observation
+			}
 		},
 		rows={
-		   {
-			   { i = 8, x = -11, y = 24 }, -- left wing
-			   { i = 9, x = 11, y = 24 }   -- right wing
+			{
+				{ i = 1, x = 0, y = -23 }, -- front camera slot
+				{ i = 2, x = 9, y = -4 }  -- internal gun
+			},
+			{
+				{ i = 3, x = 0, y = 7 },   -- centre
+				{ i = 4, x = -18, y = 7 }, -- left inner
+				{ i = 5, x = 18, y = 7 },  -- right inner
+			},
+			{
+				{ i = 6, x = -9, y = 24 }, -- left util
+				{ i = 7, x = 9, y = 24 }   -- right util
+			},
+		    {
+			   { i = 8, x = -26, y = 16 }, -- left wing
+			   { i = 9, x = 26, y = 16 },  -- right wing
+			   { i = 10, x = 0, y = 16 },  -- dorsal
 			}
 		},
 		cost={
-            [e_inventory_item.vehicle_wheel_light] = 1,
             [e_inventory_item.fuel_barrel] = 2,
-			[e_inventory_item.ammo_cruise_missile] = 2,
+			[e_inventory_item.attachment_camera_observation] = 1
         }
 	}
 }
